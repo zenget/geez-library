33d38a790efee5fcd645f145749e420d4f4468a8
Dawit Getachew <dgetachew@paga.com>
2021-06-11 01:02:46 +0300

    sample test written for book & user apis

--- geezlibrary/books/tests.py
+++ geezlibrary/books/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from books.api.serializers import BorrowHistorySerializer, BookSerializer
+from books.models import Book, BorrowHistory
+
+class BooksViewSetTestCase(APITestCase):
+    
+    list_url = reverse("books")
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="testcase",
+                                             password="dgdg12345")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_book_list_authenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_book_list_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_book_detail_retrieve(self):
+        response = self.client.get(reverse("books", kwargs={"slug": "aaaaa-9059t0"}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_avaialble_book_list(self):
+        response = self.client.get(reverse("books", kwargs={"is_available": "True"}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
--- geezlibrary/users/tests.py
+++ geezlibrary/users/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from books.api.serializers import BorrowHistorySerializer, BookSerializer
+from books.models import Book, BorrowHistory
+
+class RegistrationTestCase(APITestCase):
+    
+    def test_registration(self):
+        data = {"username": "testcase", "email": "test@geez.com",
+                "password1": "dgdg12345", "password2": "dgdg12345"}
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_registration(self):
+        data = {"username": "testcase", "email": "test@geez.com",
+                "password1": "dgdg12345"}
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class UserDetailsTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="testcase",
+                                             password="dgdg12345")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_user_detail_authenticated(self):
+        response = self.client.get(reverse("user"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["borrowed_count"], 2)
+        self.assertEqual(response.data["due_for_return_in_30_days_count"], 1)
+
